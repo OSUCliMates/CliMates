777ec4629eabdc038b036afa8ee5e40968b312d6
ErickaBSmith <erickas157@gmail.com>
2020-03-05 10:04:42 -0800

    rough beginnings to windowing

---
+++ windowing.R
@@ -0,0 +1,18 @@
+# psuedo code for windowing
+
+store current date
+
+if year = year & month <= month
+KEEP
+elseif year=year &month=month & day <day
+KEEP
+
+for (i in 1:3)
+    if year =year - i
+KEEP
+
+
+if year=year-4 & month > month
+KEEP
+if year=year-4 and month=month and day>= day 
+KEEP
